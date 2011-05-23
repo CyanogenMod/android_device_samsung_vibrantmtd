35311067608dfca9cc9525562f1dafff3d0ce021
codeworkx <daniel.hillenbrand@codeworkx.de>
2011-05-23 22:56:40 +0200

    remove "aries" from TARGET_OTA_ASSERT_DEVICE to prevent that users flash a false rom to their phone. they're really doing it.
    Change-Id: I46272cfa6b34fa73e86b42e08060415bab61e032

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -104,4 +104,4 @@ TARGET_RECOVERY_PRE_COMMAND="echo 1 > /cache/.startrecovery; sync;"
 # Include aries specific stuff
 -include device/samsung/aries-common/Android.mk
 
-TARGET_OTA_ASSERT_DEVICE := aries,vibrant,vibrantmtd,SGH-T959
+TARGET_OTA_ASSERT_DEVICE := vibrant,vibrantmtd,SGH-T959
