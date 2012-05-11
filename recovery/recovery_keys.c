2ff54fa7cf14262cd042dbf298eee84a69a38fd6
Daniel Bateman <jetison.24@gmail.com>
2012-05-10 22:03:44 -0500

    vibrantmtd: fix recovery key mapping
    Fix soft key recovery key mapping so:
    
    MENU => UP
    HOME => SELECT
    BACK => BACK
    SEARCH => DOWN
    
    Also, correct aries-keypad.kl
    
    Change-Id: I3c6f08f7ba504e9ad9820582326c92d182aa95b2

--- recovery/recovery_keys.c
+++ recovery/recovery_keys.c
@@ -4,7 +4,6 @@
 #include "common.h"
 #include "extendedcommands.h"
 
-
 int device_toggle_display(volatile char* key_pressed, int key_code) {
     int alt = key_pressed[KEY_LEFTALT] || key_pressed[KEY_RIGHTALT];
     if (alt && key_code == KEY_L)
@@ -20,25 +19,23 @@ int device_toggle_display(volatile char* key_pressed, int key_code) {
 int device_handle_key(int key_code, int visible) {
     if (visible) {
         switch (key_code) {
-            case KEY_CAPSLOCK:
-            case KEY_DOWN:
             case KEY_VOLUMEDOWN:
+            case 217: // SEARCH
                 return HIGHLIGHT_DOWN;
 
-            case KEY_LEFTSHIFT:
-            case KEY_UP:
             case KEY_VOLUMEUP:
+            case 139: // MENU
                 return HIGHLIGHT_UP;
 
-            case KEY_HOME:
+            case 158: // HOME
                 return SELECT_ITEM;
 
-            case KEY_POWER:
-                if (ui_get_showing_back_button())
-                    return SELECT_ITEM;
+            case 102: // BACK
                 if (!get_allow_toggle_display())
                     return GO_BACK;
-                break;
+
+            case KEY_POWER:
+                return SELECT_ITEM;
             default:
                 return NO_ACTION;
         }
