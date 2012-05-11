f54ff2bc9c91e34b60c63d208994458af90864e5
Daniel Bateman <jetison.24@gmail.com>
2012-05-11 02:51:24 -0500

    vibrantmtd: change keys for toggle display
    Key is now MENU + VOL UP, instead of MENU or BACK
    
    Change-Id: I9df298123448992b368f056f7e832efca47db4a0

--- recovery/recovery_keys.c
+++ recovery/recovery_keys.c
@@ -5,15 +5,7 @@
 #include "extendedcommands.h"
 
 int device_toggle_display(volatile char* key_pressed, int key_code) {
-    int alt = key_pressed[KEY_LEFTALT] || key_pressed[KEY_RIGHTALT];
-    if (alt && key_code == KEY_L)
-        return 1;
-    // allow toggling of the display if the correct key is pressed, and the display toggle is allowed or the display is currently off
-    if (ui_get_showing_back_button()) {
-        return 0;
-        //return get_allow_toggle_display() && (key_code == KEY_HOME || key_code == KEY_MENU || key_code == KEY_END);
-    }
-    return get_allow_toggle_display() && (key_code == KEY_MENU || key_code == KEY_BACK);
+    return get_allow_toggle_display() && key_pressed[139] && key_pressed[KEY_VOLUMEUP];
 }
 
 int device_handle_key(int key_code, int visible) {
